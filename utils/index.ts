bf45897875b7d68705936542eb2691a2c0e771f0
Hasun Nilupul <hasunnilupul16@gmail.com>
2023-08-21 02:57:19 +0530

    initial commit

---
+++ utils/index.ts
@@ -0,0 +1,56 @@
+import { CarProps, FilterProps } from "@/types";
+
+const apiUrl = 'https://cars-by-api-ninjas.p.rapidapi.com/v1/cars';
+
+export const fetchCars = async (filters: FilterProps) => {
+    const { manufacturer, year, model, limit, fuel } = filters;
+    const headers = {
+        'X-RapidAPI-Key': 'e0d2f0fe7emsha268c6d5763ad3ap13ab2bjsn9528e29e5220',
+        'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'
+    }
+
+    const response = await fetch(`${apiUrl}?make=${manufacturer}&year=${year}&model=${model}&limit=${limit}&fuel_type=${fuel}`, {
+        headers: headers,
+    });
+
+    const data = await response.json();
+    return data;
+}
+
+export const generateCarImageUrl = (car: CarProps, angle?: string) => {
+    const url = new URL("https://cdn.imagin.studio/getimage");
+    const { make, model, year } = car;
+
+    url.searchParams.append('customer', 'hrjavascript-mastery');
+    url.searchParams.append('make', make);
+    url.searchParams.append('modelFamily', model.split(" ")[0]);
+    url.searchParams.append('zoomType', 'fullscreen');
+    url.searchParams.append('modelYear', `${year}`);
+    // url.searchParams.append('zoomLevel', zoomLevel);
+    url.searchParams.append('angle', `${angle}`);
+
+    return `${url}`;
+}
+
+export const calculateCarRent = (city_mpg: number, year: number) => {
+    const basePricePerDay = 50;
+    const mileageFactor = 0.1;
+    const ageFactor = 0.05;
+
+    const mileageRate = city_mpg * mileageFactor;
+    const ageRate = (new Date().getFullYear() - year) * ageFactor;
+
+    const rentalRatePerDay = basePricePerDay + mileageRate + ageRate;
+
+    return rentalRatePerDay.toFixed(0);
+}
+
+export const updateSearchParams = (type: string, value: string) => {
+    const searchParams = new URLSearchParams(window.location.search);
+
+    searchParams.set(type, value);
+
+    const newPathname = `${window.location.pathname}?${searchParams.toString()}`;
+
+    return newPathname;
+}
